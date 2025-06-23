073194fd9f2509c8caf915b7293bb6a21411c964
Tiago Santos <tiago40427@gmail.com>
2025-06-23 19:13:22 -0300

    Implementando Padrão de projeto Repository

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,24 +1,50 @@
-import { PostModel } from "@/models/post/post-model";
-import { PostRepository } from "./post-repository";
+import { PostModel } from '@/models/post/post-model';
+import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+);
 
-export class JsonPostRepository implements PostRepository{
-  private async readFromDisk(){}
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk():Promise<PostModel[]>{
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8') //readFile para ler um arquivo e retornar os dados em uma string
+    const parsedJson = JSON.parse(jsonContent)
+    const {posts} = parsedJson; //para pegar os posts depois do parse, desestruturados 
+    return posts
+  }
+
   async findAll(): Promise<PostModel[]> {
-    
+   const posts = await this.readFromDisk();
+  return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if(!post) throw new Error('Post não encontrado')
+
+    return post 
   }
 }
 
-export const postRepository = new JsonPostRepository();
+export const postRepository:PostRepository = new JsonPostRepository();
 
-/*
-Instalamos 'npm i -D tsx'
-para executar e fazer testes do nosso código via terminal 
 
-para executar o código temos que digitar: npx tsx .\src\arquivo\arquivo.x
-aqui estamos verificando o que temos dentro de process.cwd()
-console.log(ROOT_DIR)
+//uma forma de fazer testes postRepository.findAll().then(jsonContent => console.log(jsonContent))
+//ou:
+(async ()=>{
 
-instalamos também a extensão code runner
-*/
+  // const posts = await postRepository.findAll(); //Para encontrar todos os posts
+  const posts = await postRepository.findById('99f8add4-7684-4c16-a316-616271db199e') //para encontrar apenas 1 post
+
+  console.log(posts)
+
+})() // ()para executar tudo
