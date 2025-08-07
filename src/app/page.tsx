b7e8c8372a516344f2209bbaa8be216d61b5d250
Tiago Santos <tiago40427@gmail.com>
2025-08-06 21:47:02 -0300

    instalando clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function HomePage(){
-   return(
+import clsx from "clsx";
+
+export default function HomePage() {
+  return (
     <>
-      <h1>HomePage!</h1>
+      <h1
+        className={clsx(
+          "text-blue-600",
+          "bg-white",
+          "font-bold",
+          "hover:bg-blue-600",
+          "hover:text-white",
+          "transition",
+          "duration-1000"
+        )}
+      >
+        HomePage!
+      </h1>
     </>
-       )
+  );
 }
