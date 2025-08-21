0f7af49b546a3ab85f42f90c80ee93c0126605b5
Tiago Santos <tiago40427@gmail.com>
2025-08-20 23:19:34 -0300

    Dark mode com tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,18 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <>
-      <header className="font-bold text-xl text-center py-8 ">
+    <div className='text-slate-900 bg-slate-100 h-screen dark:bg-slate-900 dark:text-slate-100 '>
+       <header className="font-bold text-xl text-center py-8 ">
         <h1>HEADER</h1>
       </header>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
+
       <footer className="font-bold text-xl text-center py-8 ">
         <h1>FOOTER</h1>
       </footer>
-    </>
+    </div>
   );
 }
