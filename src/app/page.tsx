0b901899cb8ee460b497d78b487e8a4796ddcd59
Tiago Santos <tiago40427@gmail.com>
2025-09-10 10:12:32 -0300

    recomeçando...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
-import { Container } from "@/components/container";
-import Header from "@/components/Header";
-import PostsList from "@/components/PostsList";
-import SpinLoader from "@/components/Spinloader";
-import { Suspense } from "react";
-
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
-      <Suspense fallback={<SpinLoader />}>
-        <PostsList />
-      </Suspense>
-
-      <footer className="font-bold text-xl text-center py-8 ">
-        <h1>FOOTER</h1>
-      </footer>
-    </Container>
+    <>
+    </>
   );
 }
