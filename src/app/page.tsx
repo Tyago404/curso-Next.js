214e6800060d1b8f05a1940c29d5ff5f19fc1f53
Tiago Santos <tiago40427@gmail.com>
2025-09-23 01:07:36 -0300

    Manipulando imagens grandes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,21 @@ export default async function HomePage() {
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
         <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
           <Image
-            className="group-hover:scale-105 transition"
+            className="group-hover:scale-105 transition w-full h-full object-cover object-center"
             src="/images/bryen_0.png"
             width={1200}
             height={720}
             alt="Titulo do post"
+            priority
           />
         </Link>
-        <div>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 block text-sm/tight" dateTime="2025-04-20">20/04/2025 10:00</time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">Lorem ipsum dolor sit amet.</Link>
+          </h1>
+
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
           sequi facilis incidunt doloremque impedit odit repellendus cum minima,
           velit ducimus voluptatum adipisci, expedita laborum vel nobis
