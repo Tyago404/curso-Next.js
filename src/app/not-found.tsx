131915c6f25157400acbb5acb6f0d5695ce7169c
Tiago Santos <tiago40427@gmail.com>
2025-10-22 01:07:42 -0300

    Adicionando componente de not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,12 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage/index.";
 
 export default function NotFoundPage() {
   return (
-    <>
-    <title>Página não encontrada</title>
-    <div
-      className={clsx(
-        "min-h-[320px] bg-slate-900 text-slate-100",
-        "mb-16 p-8 rounded-xl",
-        "flex justify-center items-center",
-        "text-center"
-      )}
-    >
-      <div>
-        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-        <p>
-          Erro 404 - A página que você está tentando acessar não existe neste
-          site.
-        </p>
-      </div>
-    </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada"
+      contentTitle="404 😅"
+      content=" Erro 404 - A página que você está tentando acessar não existe neste
+          site."
+    />
   );
 }
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,14 +2,18 @@ import clsx from "clsx";
 
 type ContainerProps = {
   children: React.ReactNode;
-}
+};
 
-export function Container({children}:ContainerProps) {
-  return(
-       <div className={clsx("text-slate-900 bg-slate-100", "min-h-screen")}>
-          <div className=" max-w-screen-lg mx-auto px-8">
-            {children}
-          </div>
+export function Container({ children }: ContainerProps) {
+  return (
+    <div
+      className={clsx(
+        "text-slate-900 bg-slate-100",
+        "min-h-screen",
+        "dark:text-slate-50 dark:bg-slate-900"
+      )}
+    >
+      <div className=" max-w-screen-lg mx-auto px-8">{children}</div>
     </div>
-  )
+  );
 }
