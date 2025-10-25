9662df20149a4a888a84f9c90f125f078271f23b
Tiago Santos <tiago40427@gmail.com>
2025-10-24 22:21:59 -0300

    chore: instalando drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
-
-  throw new Error("deu ruim!");
   return (
     <>
-
       <Suspense fallback={<SpinLoader className="min-h-20 mb" />}>
         <PostFeatured />
         <PostsList />
